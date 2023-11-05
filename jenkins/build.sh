3527fb51b46190bfaae2d15d6163178d5fd63fa1
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2023-11-05 19:17:03 -0300

    Adds instructions on readme file

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,6 +2,15 @@
 
 bash dind.sh
 
-docker build -t marcelomaidden/jenkins_with_docker .
+docker_repo=$1
+build_number=$2
 
-docker run -d --name jenkins -p 9090:8080 -it marcelomaidden/jenkins_with_docker
+docker build -t "$docker_repo/jenkins_with_docker:$build_number" .
+
+docker run --name jenkins --restart=on-failure --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 9090:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  "$docker_repo/jenkins_with_docker:$build_number"
